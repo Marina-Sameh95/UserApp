f311e967b9f6e98ec9db501c48255acf49557567
asmaa <asmaa.ashraf.oraby@gmail.com>
2020-06-09 05:19:05 +0200

    EventDetailsUpdateUIForOneImage

--- KidZoonaUserApp/KidZoonaUserApp/Scene/Event/EventViewController.swift
+++ KidZoonaUserApp/KidZoonaUserApp/Scene/Event/EventViewController.swift
@@ -8,19 +8,26 @@
 
 import UIKit
 
-class EventViewController: UIViewController , UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class EventViewController: UIViewController {
 
-    @IBOutlet weak var collectionView: UICollectionView!
-    @IBOutlet weak var sliderPage: UIPageControl!
-    @IBOutlet weak var favBtn: UIButton!
+    @IBOutlet weak var eventImage: UIImageView!
+    @IBOutlet weak var eventName: UILabel!
+    
+    @IBOutlet weak var eventDate: UILabel!
+    @IBOutlet weak var eventTime: UILabel!
+    @IBOutlet weak var eventAddress: UILabel!
+    @IBOutlet weak var eventDirectionOnMap: UIButton!
+    @IBOutlet weak var eventCost: UILabel!
+    @IBOutlet weak var eventRegisteredPeople: UILabel!
+    
+    @IBOutlet weak var availablePlacesInEvent: UILabel!
+    
+    @IBOutlet weak var desciptionTextOfEvent: UITextView!
+    
     @IBOutlet weak var registerBtn: UIButton!
     
     
-    var imageArr = [
-        UIImage(named: "event1"),
-        UIImage(named: "event2"),
-        UIImage(named: "event3")
-    ]
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,43 +41,6 @@ class EventViewController: UIViewController , UICollectionViewDelegate, UICollec
     }
     
   
-    @IBAction func favouriteBtn(_ sender: UIButton) {
-        if sender.isSelected {
-            sender.isSelected = false
-        } else {
-            sender.isSelected = true
-        }
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return imageArr.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
-        if let vc = cell.viewWithTag(111) as? UIImageView {
-            vc.image = imageArr[indexPath.row]
-        } else if let ab = cell.viewWithTag(222) as? UIPageControl{
-            ab.currentPage = indexPath.row
-        }
-        return cell
-    }
   
-        func collectionView(_collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets{
-            return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        }
-    
-    func collectionView(_collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizwForItemAt indexPath: IndexPath)-> CGSize {
-        let size = UIScreen.main.bounds
-        return CGSize(width: size.width, height: size.height)
-    }
-    
-    func collectionView(_collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int)-> CGFloat {
-        return 0.0
-    }
-    
-    func collectionView(_collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int)-> CGFloat{
-        return 0.0
-    }
 
 }

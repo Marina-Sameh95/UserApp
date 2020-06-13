bb38841a88f33436502bd51cf7c221ce4f73705b
asmaa <asmaa.ashraf.oraby@gmail.com>
2020-06-13 18:11:08 +0200

    EventUpdate

--- KidZoonaUserApp/KidZoonaUserApp/Scene/Home/OffersCellCollectionViewCell.swift
+++ KidZoonaUserApp/KidZoonaUserApp/Scene/Home/OffersCellCollectionViewCell.swift
@@ -22,7 +22,6 @@ class OffersCellCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var courseDiscountInOffersCollection: UILabel!
     
     
-    @IBOutlet weak var favoriteBtnInOffersCollection: UIButton!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -30,20 +29,6 @@ class OffersCellCollectionViewCell: UICollectionViewCell {
         offerImageInOffersCollection.layer.cornerRadius = 15
     }
     
-    @IBAction func favoriteBtnInOffersCollAction(_ sender: Any) {
-        
-        print("1 2 3")
-        guard let uid = Auth.auth().currentUser?.uid else {return}
-        let ref = Database.database().reference().child("User").child(uid).child("WishList")
-        let wishListValue = ["CourseId" : "123"]
-        ref.updateChildValues(wishListValue, withCompletionBlock: { (error, ref ) in
-            if let error = error {
-                print("failed to update/push data in Database", error.localizedDescription)
-            }else{
-                print("suessfully update Data in DataBase")
-            }
-        })
-    }
     
     
 }
